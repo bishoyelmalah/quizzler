6c8d33e52a6ece150a43440d1571d2d4720a8c0f
bishoyelmalah <bishoy.elmalah@gmail.com>
2025-02-17 12:39:41 +0200

    Move quiz logic to QuizPage class and update imports for better organization

---
+++ lib/quiz_page.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'questions.dart';
+import 'score_page.dart';
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  // List<String> questions = [
+  //   "You can lead a cow upstairs, but not downstairs.",
+  //   "Approximately one quarter of human bones are in the feet",
+  //   "A slug's blood is green."
+  // ];
+
+  // List<bool> answers = [false, true, true];
+
+  List<Question> questionBank = [
+    Question("You can lead a cow upstairs, but not downstairs.", false),
+    Question("Approximately one quarter of human bones are in the feet", true),
+    Question("A slug's blood is green.", true)
+  ];
+
+  Icon correct = Icon(
+    Icons.check_box,
+    color: Colors.green,
+  );
+
+  Icon wrong = Icon(
+    Icons.close,
+    color: Colors.red,
+  );
+
+  int questionNumber = 0;
+  int score = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                questionBank[questionNumber].questionText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (questionBank[questionNumber].questionAnswer) {
+                      scoreKeeper.add(correct);
+                      score++;
+                    } else {
+                      scoreKeeper.add(wrong);
+                    }
+                    if (questionNumber < questionBank.length - 1) {
+                      questionNumber++;
+                    } else {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ScorePage(score: score)));
+                    }
+                  });
+                },
+                style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.green),
+                    shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0)))),
+                child: Text(
+                  "True",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                )),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (!questionBank[questionNumber].questionAnswer) {
+                      scoreKeeper.add(correct);
+                      score++;
+                    } else {
+                      scoreKeeper.add(wrong);
+                    }
+                    if (questionNumber < questionBank.length - 1) {
+                      questionNumber++;
+                    } else {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ScorePage(score: score)));
+                    }
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.red),
+                  shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0))),
+                ),
+                child: Text(
+                  "False",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                )),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
